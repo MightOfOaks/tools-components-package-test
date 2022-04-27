6ab7387b4914ea7e331e852e12aaf8effb20e6f2
Serkan Reis <serkanreis@gmail.com>
2022-04-27 19:35:55 +0300

    TailwindCSS update

---
+++ README.md
@@ -0,0 +1,1 @@
+# Shared component library for -tools projects
--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import tailwind from 'rollup-plugin-tailwindcss';
+
 
 const packageJson = require("./package.json");
 
@@ -25,19 +27,26 @@ export default [
     ],
     plugins: [
       peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      tailwind({
+        input: './src/index.css', // required
+        // Tailor the emitted stylesheet to the bundle by removing any unused CSS
+        // (highly recommended when packaging for distribution).
+        purge: false,
+      }),
       postcss({
         config: {
           path: "./postcss.config.js",
         },
         extensions: [".css"],
         minimize: true,
+        extract: true,
         inject: {
           insertAt: "top",
         },
       }),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
     ],
   },
@@ -45,6 +54,5 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/],
   },
 ];
